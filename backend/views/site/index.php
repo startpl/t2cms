22656844fe779e9145c8d00a9e316f78eb9163d6
koperdog <checnev1997@yandex.ru>
2020-09-16 12:54:50 +0500

    Added info to admin dashboard

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,19 +11,22 @@ $this->title = 'My T2cms application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
         <div>
-            <?php           
-                $model = startpl\t2cmsblog\helpers\CategoryHelper::get(2);
-//                debug(\startpl\t2cms\acf\helpers\ACF::getFields($model))?>
-            <?php debug(
- \t2cms\meta\Meta::t('[[city]] was beautiful, but [[robots]] is empty :c')
-            );?>
-            <?= \startpl\yii2NestedSetsMenu\Menu::widget([
-                'items' => \t2cms\menu\services\nestedSets\MenuArray::getItems('top_menu'),
-                'options' => ['id'=>'top-menu', 'class' => 'top_menu'],
-                'encodeLabels'=>false,
-                'activateParents'=>true,
-                'activeCssClass'=>'active',
-            ]);?>
+            Подмена переменных настроек в строке:
+            <code>
+                \t2cms\meta\Meta::t('Main city is - [[city]]')
+            </code>
+            <br/>
+            Получение элементов меню:
+            <code>
+                \t2cms\menu\services\nestedSets\MenuArray::getItems('top_menu')
+                где top_menu - имя меню
+            </code>
+            <br/>
+            <div>Blog Helpers смотреть в <code>@vendor/startpl/t2cms-blog</code></div>
+            <div>ACF Helpers смотреть в <code>cms/modules/t2cms-acf</code></div>
+            
+            <p>Вся система построенна на слоях Service -> Repository</p>
+            
         </div>
     </div>
 </div>
